c9f84636b1adfa2370e190ed24eb2b57a2ffbcbb
LWE27 <lee.westrich@googlemail.com>
2026-07-10 10:06:17 +0200

    61. Exposing Variables to Blueprint

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -20,13 +20,13 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	
-private:
-	UPROPERTY(VisibleAnywhere)
-	float RunningTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Sine Parameters")
+	float Amplitude = 0.25f;	
 	
-	UPROPERTY(EditAnywhere)
-	float Amplitude = 0.25f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Sine Parameters")
+	float TimeConstant = 5.f;	
 
-	UPROPERTY(EditAnywhere)
-	float TimeConstant = 5.f;
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
+	float RunningTime;
 };
