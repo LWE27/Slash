e1332b47ed6c332c48eefd63372b01309b06728e
LWE27 <lee.westrich@googlemail.com>
2026-07-10 17:46:25 +0200

    70. Skeletal Meshe Components

--- Source/Slash/Private/Pawns/Bird.cpp
+++ Source/Slash/Private/Pawns/Bird.cpp
@@ -3,6 +3,7 @@
 
 #include "Pawns/Bird.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
 
 ABird::ABird()
 {
@@ -12,24 +13,23 @@ ABird::ABird()
 	Capsule->SetCapsuleHalfHeight(20.f);
 	Capsule->SetCapsuleRadius(15.f);
 	SetRootComponent(Capsule); // Safe variant ; Same as RootComponent = Capsule
+
+	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
+	BirdMesh->SetupAttachment(GetRootComponent());
 }
 
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 void ABird::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
-
