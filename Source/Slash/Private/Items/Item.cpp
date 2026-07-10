beaa1c6da82ca39c142c499352b9a1d25e76f41c
LWE27 <lee.westrich@googlemail.com>
2026-07-10 10:36:13 +0200

    62.Exposing Functions to Blueprint

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -15,15 +15,25 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 }
 
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
+float AItem::TransformedCos()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);
+}
+
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 	RunningTime += DeltaTime; // Kinda like a stopwatch
 	// Amplitude Height .25f & Speed 5.f
-	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
+	//float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
 	
-	AddActorWorldOffset(FVector(0.0f, 0.0f, DeltaZ));
+	//AddActorWorldOffset(FVector(0.0f, 0.0f, DeltaZ));
 	
 	DRAW_SPHERE_SingleFrame(GetActorLocation());
 	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
