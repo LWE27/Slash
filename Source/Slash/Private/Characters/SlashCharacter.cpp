b8b325fb66c638918b3f7ee8f5a444fc1bf1b0bc
LWE27 <lee.westrich@googlemail.com>
2026-07-12 13:52:35 +0200

    85. Controller Directions (EIS)

--- Source/Slash/Private/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -45,11 +45,14 @@ void ASlashCharacter::Move(const FInputActionValue& Value)
 {
 	const FVector2D MovementVector = Value.Get<FVector2D>();
 	
-	const FVector Forward = GetActorForwardVector();
-	AddMovementInput(Forward, MovementVector.Y);
-
-	const FVector Right = GetActorRightVector();
-	AddMovementInput(Right, MovementVector.X);
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(RightDirection, MovementVector.X);
 }
 
 void ASlashCharacter::Look(const FInputActionValue& Value)
