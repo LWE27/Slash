3c040e0ff4919c14ef1fc640706b70d3700f885e
LWE27 <lee.westrich@googlemail.com>
2026-07-12 21:46:21 +0200

    90. The Anim Instance

---
+++ Source/Slash/Public/Characters/SlashAnimInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlashAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API USlashAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// Äquivalent zum BP-Event: Blueprint Initialize Animation
+	virtual  void NativeInitializeAnimation() override;
+	
+	// Äquivalent zum BP-Event: Blueprint Update Animation
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+	
+	UPROPERTY(BlueprintReadOnly) // Zeigt Variable links bei den BPs an
+	class ASlashCharacter* SlashCharacter;
+	
+	UPROPERTY(BlueprintReadOnly, Category=Movement) // Zeigt Variable links bei den BPs an
+	class UCharacterMovementComponent* SlashCharacterMovement;
+	
+	UPROPERTY(BlueprintReadOnly, Category=Movement) // Zeigt Variable links bei den BPs an
+	float GroundSpeed;
+};
